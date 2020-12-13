7e5bff035e71b38e2034660b8eb6a2e4f874ed77
luxzg <pribanic.luka@gmail.com>
2020-12-13 16:06:31 +0100

    Create ps1.php

---
+++ PHP/ps1.php
@@ -0,0 +1,4 @@
+# Placeholder
+
+ps1.php
+This file will be linked after new client is submitted to the database, and clicking the link you will receive a download of <clientname>.ps1 script. Running this script will configure Hyper-V and start the installation, writing out a live log in the process so you can monitor the status of your installation. Once installation is finished it will let you know, and restart and reconfigure the VM.
