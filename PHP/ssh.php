ecaaaf076efe7b8cee0bb0f80de6ab6fc0b26be6
luxzg <pribanic.luka@gmail.com>
2020-12-13 16:07:11 +0100

    Create ssh.php

---
+++ PHP/ssh.php
@@ -0,0 +1,4 @@
+# Placeholder
+
+ssh.php
+Helper file containing calls to phpseclib, PHP library that offers SSH and SCP actions via PHP. We use it to restart DHCP service (from pxeclients.php) as well as for assorted actions in manage.php (reboot, fetching screenshots, etc).
