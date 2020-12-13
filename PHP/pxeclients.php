cc05e5295f40ca5e1144bc2b4084a21b45ae9eb5
luxzg <pribanic.luka@gmail.com>
2020-12-13 16:05:09 +0100

    Create pxeclients.php

---
+++ PHP/pxeclients.php
@@ -0,0 +1,4 @@
+# Placeholder
+
+pxeclients.php
+This is simple entry form where you enter all the details and submit them to the database. In turn same file will call other PHP files that perform special functions.
