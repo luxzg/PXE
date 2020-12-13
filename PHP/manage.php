d6ce87794e8d8eec9cee385a1cda4d861ef7fa4a
luxzg <pribanic.luka@gmail.com>
2020-12-13 16:06:50 +0100

    Create manage.php

---
+++ PHP/manage.php
@@ -0,0 +1,4 @@
+# Placeholder
+
+manage.php
+This is very basic management page for all clients in the database. It will list all the clients, their details, and offer action links for each client, such as reboot, view info, view config, etc. This page is NOT required for system to work, but it is a nice place to view basic information without digging into database or configuration files.
