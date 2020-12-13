ed811de273c9d46d54ec3ae0d4dcbf8d985d409c
luxzg <pribanic.luka@gmail.com>
2020-12-13 16:06:03 +0100

    Create pxecfg.php

---
+++ PHP/pxecfg.php
@@ -0,0 +1,4 @@
+# Placeholder
+
+pxecfg.php
+This file will pull all data from MySQL DB and (re)generate all files in pxelinux.cfg/ folder (except default !), in turn creating individual boot config menu for each client (based on MAC address).
