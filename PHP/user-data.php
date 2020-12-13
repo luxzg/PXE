06eb69733138ce6e497ab6de49b3a6ba73493be3
luxzg <pribanic.luka@gmail.com>
2020-12-13 16:07:31 +0100

    Create user-data.php

---
+++ PHP/user-data.php
@@ -0,0 +1,4 @@
+# Placeholder
+
+user-data.php
+File that we call from subiquity using curl and GET parameter mac=<client-mac-address>. It will pull data from database for that unique MAC entry, and compile a user-data (autoinstall.yaml) file with settings such as initial username, password, static networking details, hostname, etc.
